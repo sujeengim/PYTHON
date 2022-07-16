b77c6f89e4bf4c02720c884bce9059ffee34fce8
김수진 <ksj030120@gmail.com>
2022-07-16 12:33:58 +0900

    CodeUp 시작

---
+++ codeup/기초100제/6017.py
@@ -0,0 +1,7 @@
+# [기초-입출력] 문장 1개 입력받아 3번 출력하기
+# 정수(integer), 실수, 문자(character), 문자열(string) 등 1개만 입력받아 한 줄로 3번 출력해보자.
+
+x = input()
+# print(x, x, x)
+for i in range(3):
+    print(x, end=' ')
---
+++ codeup/기초100제/6019.py
@@ -0,0 +1,4 @@
+# 연월일 입력받아 순서 바꿔 출력하기
+
+y, m, d = map(int, input().split('.'))
+print(d, m, y, sep='-')
