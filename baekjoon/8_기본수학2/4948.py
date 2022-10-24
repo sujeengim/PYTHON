aa9517c11e566768f13f2a74462753f74eedf0ad
김수진 <ksj030120@gmail.com>
2022-10-25 08:26:00 +0900

    빠르게 소수 찾기

--- baekjoon/8_기본수학2/4948.py
+++ baekjoon/8_기본수학2/4948.py
@@ -1,3 +1,4 @@
+## 시간초과
 import math
 
 def prime(p):
@@ -23,3 +24,37 @@ while True:
         prime_cnt = len(prime(n))
 
         print(prime_cnt)
+
+## 답
+import sys
+
+#소수 찾기
+def decimal(x):
+    if x == 1:
+        return False
+    for k in range(2, int(x**0.5)+1):
+        if x % k == 0:
+            return False
+    return True
+
+#가능한 모든 수가 소수인지 확인
+decimal_nums = []
+for i in range(2, 246912):
+    if decimal(i):
+        decimal_nums.append(i)
+
+#반복문을 통해 케이스를 확인
+while True:
+    n = int(sys.stdin.readline())
+    cnt = 0
+
+    #입력 받은 수가 0이면 반복을 멈춤
+    if n == 0:
+        break
+
+    #소수인 수를 반복하여 구간에 있는지 확인
+    for j in decimal_nums:
+        #구간에 있으면 카운트
+        if n < j <= 2*n:
+            cnt+=1
+    print(cnt)
