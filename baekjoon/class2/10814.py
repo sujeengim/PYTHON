cacdec2f81ba942da51e468c4a1953b099b22535
김수진 <ksj030120@gmail.com>
2022-10-28 22:45:01 +0900

    정렬 좀 재밌었다

--- baekjoon/class2/10814.py
+++ baekjoon/class2/10814.py
@@ -1,7 +1,18 @@
-while True:
-    o, p, q = map(int, input().split())
-    if o == 0 and p ==0 and q == 0:
-        break
-    list = [o,p,q]
-    m = max(list)
-    
+#나이 오름, 가입일 오름
+
+N = int(input())
+
+a = []
+n = []
+s = []
+
+for _ in range(N):
+    age, name = input().split()
+    age = int(age) #w정수형으로 따로 만들어주기
+    s.append([age, name])
+
+s.sort(key = lambda x: x[0])
+#여러정렬기준 : key = lambda x : (x[0], x[1]) -> 우선 x[0]기준 정렬, 후 x[1]정렬
+
+for i in s:
+    print(i[0], i[1])
