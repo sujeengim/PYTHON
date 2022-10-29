7c7737741adc6a56f65b0551fcfc4e823e4e7ad0
김수진 <ksj030120@gmail.com>
2022-10-29 10:26:28 +0900

    pypy로 하면 빠름

--- baekjoon/class2/2751.py
+++ baekjoon/class2/2751.py
@@ -1,3 +1,4 @@
+# 시간초과
 N = int(input())
 
 list = []
@@ -10,4 +11,15 @@ list.sort()
 tp = tuple(list) #중복 제거
 
 for i in tp:
+    print(i)
+
+# pypy로 풀기
+N = int(input())
+arr = []
+
+for i in range(N):
+    arr.append(int(input()))
+
+arr.sort()
+for i in arr:
     print(i)
