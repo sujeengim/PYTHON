ae37ed67154727c0e19369aa2c97546aa475c72a
김수진 <ksj030120@gmail.com>
2022-11-04 00:11:13 +0900

    맨 왼쪽 수가 0이면 무시

--- baekjoon/class2/1259.py
+++ baekjoon/class2/1259.py
@@ -2,6 +2,11 @@ while True:
     n = input()
     if n=='0':
         break
+    if n[0] == '0':
+        for i in range(1,len(n)):
+            if n[i]!='0':
+                n = n[i:]
+                break
     n2 = n[::-1]
     if n2==n:
         print('yes')
