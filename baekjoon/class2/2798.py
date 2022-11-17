9aa61366e55246eae7a12c25667cf9047b984fa4
김수진 <ksj030120@gmail.com>
2022-11-17 23:57:13 +0900

    반복부분 다시 보기

--- baekjoon/class2/2798.py
+++ baekjoon/class2/2798.py
@@ -1,28 +1,41 @@
 #런타임에러
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
+#
+# lst = list(map(int, input().split()))
+#
+# def fac(x):
+#     ans = 1
+#     for _ in range(1,x+1):
+#         ans *= x
+#         x -= 1
+#     return ans
+#
+# s = [0] * fac(n)
+#
+# index = 0
+#
+# for a in lst:
+#     for b in lst:
+#         if (a==b) or (a+b > m):
+#             continue
+#         for c in lst:
+#             if (a==c) or (b==c) or (a+b+c > m):
+#                 continue
+#             s[index] = a+b+c
+#             index += 1
+#
+# print(max(s))
 
+n, m = map(int, input().split())
 lst = list(map(int, input().split()))
 
-def fac(x):
-    ans = 1
-    for _ in range(1,x+1):
-        ans *= x
-        x -= 1
-    return ans
+ans = 0
 
-s = [0] * fac(n)
-
-
-index = 0
-
-for a in lst:
-    for b in lst:
-        if (a==b) or (a+b > m):
-            continue
-        for c in lst:
-            if (a==c) or (b==c) or (a+b+c > m):
+for a in range(n):
+    for b in range(a+1, n):
+        for c in range(b+1, n):
+            if lst[a] + lst[b] + lst[c] > m:
                 continue
-            s[index] = a+b+c
-            index += 1
+            ans = max(ans, lst[a] + lst[b] + lst[c])
 
-print(max(s))
+print(ans)
