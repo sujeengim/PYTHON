592fad293ac2d76be544252c3086ce4ce5ad8e4f
김수진 <ksj030120@gmail.com>
2022-12-30 01:08:34 +0900

    행렬곱셈 푸는중

--- baekjoon/silver5/2740.py
+++ baekjoon/silver5/2740.py
@@ -2,8 +2,19 @@
 
 import sys
 
-n, m = map(int, sys.stdin.readline())
-a = [list(map(int, sys.stdin.readline())) for i in range(n)]
-m, k = map(int, sys.stdin.readline())
-b = [list(map(int, sys.stdin.readline())) for i in range(m)]
+n, m = map(int, sys.stdin.readline().split())
+a = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+m, k = map(int, sys.stdin.readline().split())
+b = [list(map(int, sys.stdin.readline().split())) for i in range(m)]
 
+ans = [[]*n]
+print('ans : ',ans)
+
+for i in range(n): #a행만큼, a의 행 차례로 선택
+    x = []
+    for j in range(k): #b열만큼, b의 열 차례로 선택
+        s = 0
+        for k in range(m): #b행만큼, a행 요소 차례로 선택
+            s += a[i][k]*b[k][j]
+        x.append(s)
+    ans.append(x)
