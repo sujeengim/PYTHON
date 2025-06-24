d1375e8f08d92012ff6ec675ba96d5d5b01c4e72
sujinkim <ksj030120@gmail.com>
2025-06-25 02:41:18 +0900

    Update 14425.py

--- baekjoon/14_집합과맵/14425.py
+++ baekjoon/14_집합과맵/14425.py
@@ -1,4 +1,5 @@
 n, m = map(int, input().strip().split(' '))
+cnt = 0
 
 nlist = [0] * n 
 mlist = [0] * m
@@ -7,4 +8,10 @@ for i in range(n):
     nlist[i] = input()
 
 for i in range(m):
-    mlist = input()
+    mlist[i] = input()
+
+for word in mlist:
+    if word in nlist:
+        cnt+=1
+
+print(cnt)
