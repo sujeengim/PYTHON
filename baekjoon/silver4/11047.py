ef61b9f27b601daace2d838d204fe1809bb47285
Sujin Kim <ksj030120@gmail.com>
2025-07-08 23:36:31 +0900

    Update 11047.py

--- baekjoon/silver4/11047.py
+++ baekjoon/silver4/11047.py
@@ -1,15 +1,33 @@
+#11047
 import sys
 
 n, k = map(int, input().strip().split())
-nlist = []
-gaplist = []
-
-for _ in range(n):
-    nlist.append(sys.stdin.readline().strip())
-    gaplist.append(abs(k - int(nlist[-1])))
-
-gaplist.sort() 
+nlist = []  # 동전 종류
+gap = k
+cnt = 0
 
 for i in range(n):
-    if gaplist[i] == 0:
-        print(n+1)
+    na = int(sys.stdin.readline().strip())
+    nlist.append(na)
+
+'''
+n: 동전의 종류 수
+k: 동전의 총합 목표
+nlist[-1] : 처음 목표에 가장 가까운 동전
+gap : 현재 목표와 차이
+cnt : 사용된 동전 개수 
+'''
+
+nlist.sort(reverse=True)  
+
+# gap 줄여가기 
+for i in range(len(nlist)):
+    while gap > 0:
+        if gap - nlist[i] >= 0:
+            u = gap//nlist[i]
+            gap -= nlist[i] * u
+            cnt += u
+        else:
+            break
+
+print(cnt)
